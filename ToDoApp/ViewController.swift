518719b214fa889c1a4f7fe48432f466f6eae244
Pavel Chirkov <ienglishman187@gmail.com>
2021-08-31 22:34:10 +0300

    added button

--- ToDoApp/ViewController.swift
+++ ToDoApp/ViewController.swift
@@ -13,7 +13,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         title = "Hello"
-        view.backgroundColor = .red
+        view.backgroundColor = .darkGray
     }
     
 
